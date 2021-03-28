7f84e4a640549c49bb3de36f29e37181719914f0
brianjos <brianjos@ualberta.ca>
2021-03-28 15:12:18 -0600

    Add test.py for assignment testing purposes

---
+++ test.py
@@ -0,0 +1,33 @@
+import unittest
+from yfinance.base import TickerBase
+
+class TickerHistoryTestCase(unittest.TestCase):
+	"""Various unit tests for yfinance's base.py history function.
+
+	https://docs.python.org/3.9/library/unittest.html
+	"""
+	
+	def setUp(self):
+		"""Set up mock TickerBase object to test its history function"""
+		self.mockTickerBase = TickerBase("ABTC-USD.SW")
+
+	def test_example_1(self):
+		"""Testing if printing history returns an output"""
+		history_df = self.mockTickerBase.history()
+
+		history_string = history_df.to_string()
+		print(history_string, "\n") # debugging purposes to see dataframe
+
+		self.assertNotEqual(None, history_string) # validate non-empty dataframe
+
+	def test_hundred_year_range(self):
+		"""Testing if an error is thrown when exceeding a 100 year data range"""
+		history_df = self.mockTickerBase.history(period='max')
+
+		history_string = history_df.to_string()
+		print(history_string, "\n") # debugging purposes to see dataframe
+
+		self.assertIn("Empty DataFrame", history_string) # validate empty dataframe due to error thrown
+
+if __name__ == '__main__':
+	unittest.main()
